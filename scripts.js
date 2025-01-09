f960539bccb664598f3aeec7b61a1156e9b3ea86
matheus <matheusbellocorrea1@gmail.com>
2025-01-09 20:09:56 +0100

    Adicionado botão Voltar ao Topo

--- scripts.js
+++ scripts.js
@@ -42,3 +42,21 @@ function setLanguage(lang) {
 
 // Define o idioma padrão como Italiano
 setLanguage('it');
+
+// Mostra o botão "Voltar ao Topo" quando a página for rolada para baixo
+window.onscroll = function () {
+    const btnTopo = document.getElementById("btnTopo");
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        btnTopo.style.display = "block"; // Exibe o botão
+    } else {
+        btnTopo.style.display = "none"; // Esconde o botão
+    }
+};
+
+// Função para rolar suavemente ao topo
+function voltarAoTopo() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth", // Rolagem suave
+    });
+}
