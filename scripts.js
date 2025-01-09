f902dc0e44965e1d6163f65e2886656d5f2ae794
matheus <matheusbellocorrea1@gmail.com>
2025-01-09 19:39:18 +0100

    Correções finais do dropdown e estilo

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ const translations = {
         'Su di me': 'Su di me',
         'Qui puoi raccontare la tua carriera professionale e i tuoi obiettivi.': 'Qui puoi raccontare la tua carriera professionale e i tuoi obiettivi.',
         'Le mie Implementazioni': 'Le mie Implementazioni',
-        'Breve descrizione del progetto.': 'Breve descrizione del progetto.',
+        'Breve descrizione del progetto.': 'Breve descrizione del projeto.',
         'Contatti': 'Contatti',
         'Contattami via e-mail o visita il mio LinkedIn.': 'Contattami via e-mail o visita il mio LinkedIn.',
         'Tutti i diritti riservati.': 'Tutti i diritti riservati.'
@@ -16,12 +16,17 @@ const translations = {
         'Su di me': 'Sobre Mim',
         'Qui puoi raccontare la tua carriera professionale e i tuoi obiettivi.': 'Aqui você pode contar sua trajetória profissional e seus objetivos.',
         'Le mie Implementazioni': 'Minhas Implementações',
-        'Breve descrizione del progetto.': 'Descrição breve do projeto.',
+        'Breve descrizione del projeto.': 'Descrição breve do projeto.',
         'Contatti': 'Contato',
         'Contattami via e-mail o visita il mio LinkedIn.': 'Entre em contato por e-mail ou visite meu LinkedIn.',
         'Tutti i diritti riservati.': 'Todos os direitos reservados.'
     }
 };
+
+document.getElementById('languageDropdown').addEventListener('click', function () {
+    const dropdownMenu = this.nextElementSibling;
+    dropdownMenu.classList.toggle('show'); // Alterna a visibilidade
+});
 
 function setLanguage(lang) {
     document.querySelectorAll('[data-lang]').forEach(el => {
