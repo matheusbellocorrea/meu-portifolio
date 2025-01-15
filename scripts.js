96799d5798a26ba4afbb9110d18aae956ad4525c
matheus <matheusbellocorrea1@gmail.com>
2025-01-15 22:40:07 +0100

    Banner 19

--- scripts.js
+++ scripts.js
@@ -15,3 +15,20 @@ function voltarAoTopo() {
         behavior: 'smooth' // Animação suave ao rolar
     });
 }
+
+
+function copiarEmail() {
+    const email = "matheusbellocorrea1@gmail.com"; // Substitua pelo seu endereço de e-mail
+    navigator.clipboard.writeText(email).then(() => {
+        // Exibe a mensagem de confirmação
+        const mensagem = document.getElementById("email-copiato");
+        mensagem.style.display = "block";
+
+        // Oculta a mensagem após 2 segundos
+        setTimeout(() => {
+            mensagem.style.display = "none";
+        }, 2000);
+    }).catch(err => {
+        console.error("Erro ao copiar o e-mail: ", err);
+    });
+}
